dcc4acd9fdbbde04af51e509ff698fe4ecf8ad24
k1te <kmiller343@gmail.com>
2020-11-01 14:58:18 -0600

    wrote put wrote for devouring burger

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -22,5 +22,17 @@ router.post("/api/burgers", function (req, res) {
   });
 });
 
+router.put("/api/burgers/:id", function(req, res) {
+  let condition = "id = " + req.params.id;
+
+  burger.update({devoured: req.body.devoured}, condition, function(result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  })
+})
+
 
 module.exports = router;
