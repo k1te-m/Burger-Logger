400245c6b4310e49719f3674285fe2d899121781
k1te <kmiller343@gmail.com>
2020-11-01 13:18:26 -0600

    wrote burger model with orm/exported

---
+++ controllers/burgerController.js
@@ -0,0 +1,21 @@
+const orm = require("../config/orm");
+
+const burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+    create: function(columns, values, cb) {
+        orm.create("burgers", columns, values, function(res) {
+            cb(res);
+        });
+    },
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
