0ab0e7c0506ecb1d3f6068bf193e99400c28257e
k1te <kmiller343@gmail.com>
2020-11-01 14:58:48 -0600

    wrote ajax call for put method

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -10,5 +10,25 @@ $(function() {
             console.log("Created new burger.");
             location.reload();
         })
+    });
+    $(".devour-it").on("click", function(event) {
+        let id = $(this).data("id");
+        let newState = {
+            devoured: true
+        }
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newState
+        }).then(function(){
+            console.log("DEVOURED!")
+            location.reload();
+        })
+
+        
+
+       
+
+        
     })
 })
