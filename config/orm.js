8a5e64b0cc949cf5a30f1450c1921cb019252636
k1te <kmiller343@gmail.com>
2020-11-01 13:12:24 -0600

    copied helper functions/ wrote all/create methods

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,70 @@
 const connection = require("../config/connection.js");
+
+//Helper Functions taken from Class Activities
+function printQuestionMarks(num) {
+    let arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    let arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+const orm = {
+    all: function(tableInput, cb) {
+        let queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(error, results) {
+            if (error) {
+                throw error;
+            }
+            cb(results);
+        });
+    },
+    create: function(table, column, values, cb) {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        connection.query(queryString, values, function(error, results) {
+            if (error) {
+                throw error;
+            }
+            cb(results);
+        });
+    },
+
+
+
+
+
+
+
+}
