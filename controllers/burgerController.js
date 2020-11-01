631d9a56d6d5a5bbfbb98f52de6c129686f910e2
k1te <kmiller343@gmail.com>
2020-11-01 14:00:44 -0600

    wrote post method/adjusted get method

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -9,10 +9,17 @@ const burger = require("../models/burger");
 router.get("/", function(req, res) {
     burger.all(function(data) {
         var hbsObject = {
-            burger: data
+            burgers: data
         };
+        console.log(hbsObject);
         res.render("index", hbsObject);
     });
+});
+
+router.post("/api/burgers", function (req, res) {
+  burger.create(["name"], [req.body.name], function (results) {
+    res.json({ id: results.insertId });
+  });
 });
 
 
